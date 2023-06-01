81724f96da2b2b294a444ac84f9120c35e5b745f
Victor Medrado <victor@agenciaexplorer.com.br>
2023-06-01 19:37:55 -0300

    Finalizando Homepage (Mobile)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,30 @@
 $(document).ready(
     function () {
-        $('.carousel-cards').slick({
-            dots: true
+        $('.carousel-products').slick({
+            dots: true,
+            arrows: false,
+            appendDots: $('.carousel-products-dots')
         });
     }
 );
+
+$(window).on('load resize orientationchange', function () {
+    $('.carousel-cards').each(function () {
+        var $carousel = $(this);
+
+        if ($(window).width() > 768) {
+            if ($carousel.hasClass('slick-initialized')) {
+                $carousel.slick('unslick');
+            }
+        }
+
+        else {
+            if (!$carousel.hasClass('slick-initialized')) {
+                $carousel.slick({
+                    dots: true,
+                    mobileFirst: true,
+                });
+            }
+        }
+    });
+})

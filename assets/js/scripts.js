a188da6a5d8fe331706e18c07bb1b341bd1f77ba
Victor Medrado <victor@agenciaexplorer.com.br>
2023-06-02 18:28:52 -0300

    Ajustes CSS e Menu responsivo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,3 @@
-$(document).ready(
-    function () {
-        $('.carousel-products').slick({
-            dots: true,
-            arrows: false,
-            appendDots: $('.carousel-products-dots')
-        });
-    }
-);
-
 $(window).on('load resize orientationchange', function () {
     $('.carousel-cards').each(function () {
         var $carousel = $(this);
@@ -27,4 +17,46 @@ $(window).on('load resize orientationchange', function () {
             }
         }
     });
+
+     $('.carousel-products').each(function() {
+        var $carousel = $(this);
+
+        if ($(window).width() > 768) {
+            if ($carousel.hasClass('slick-initialized')) {
+                $carousel.slick('unslick');
+            }
+        }
+
+        else {
+            if (!$carousel.hasClass('slick-initialized')) {
+                $carousel.slick({
+                    dots: true,
+                    mobileFirst: true,
+                    arrows: false,
+                    appendDots: $('.carousel-products-dots')
+                });
+            }
+        }
+     })
+
+    //  alert("Este é um site em desenvolvimento. Ao final do dia, teremos uma nova versão por aqui. Enquanto isso, que tal me enviar um feedback no Slack, sobre sua experiência? Obrigado! :D");
 })
+
+$(function() {
+    var header = $(".navbar");
+    var toggler = $(".navbar-toggler");
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 80) {
+            header.removeClass('fixedHeader').addClass("scrollHeader");
+        } else {
+            header.removeClass("scrollHeader").addClass('fixedHeader');
+        }
+    });
+
+    $(toggler).click(function() {
+        $(header).toggleClass('active'); 
+    })
+});
